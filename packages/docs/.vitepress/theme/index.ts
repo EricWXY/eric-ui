00d32b2b03dfd7033e7cbdc76058ff9739c080c6
EricWXY <ericwxy@foxmail.com>
2024-04-01 17:07:04 +0800

    docs:README

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import { type App } from "vue";
 import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 
 import "@vitepress-demo-preview/component/dist/style.css";
-import "./custom.css";
 
 export default {
   ...DefaultTheme,
