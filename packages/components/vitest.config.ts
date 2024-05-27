87554736470ae38c6f7bc099cc00ad8e555d58ff
EricWXY <ericwxy@foxmail.com>
2024-05-27 14:53:45 +0800

    chore:修复 vitest.config 使其适配条件编译

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -1,13 +1,18 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  define: {
+    PROD: JSON.stringify(false),
+    DEV: JSON.stringify(false),
+    TEST: JSON.stringify(true),
+  },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+});
